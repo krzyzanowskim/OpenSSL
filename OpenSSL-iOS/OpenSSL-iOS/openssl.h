a7c68aac5f0e053e7ac079c7cee51bf2ad817b3f
Josip Cavar <josipcavar04@gmail.com>
2016-07-15 20:57:35 +0200

    add workspace for building frameworks

---
+++ OpenSSL-iOS/OpenSSL-iOS/openssl.h
@@ -0,0 +1,19 @@
+//
+//  OpenSSL-iOS.h
+//  OpenSSL-iOS
+//
+//  Created by Josip Cavar on 15/07/16.
+//  Copyright © 2016 krzyzanowskim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpenSSL-iOS.
+FOUNDATION_EXPORT double OpenSSL_iOSVersionNumber;
+
+//! Project version string for OpenSSL-iOS.
+FOUNDATION_EXPORT const unsigned char OpenSSL_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenSSL_iOS/PublicHeader.h>
+
+
---
+++ OpenSSL-macOS/OpenSSL-macOS/openssl.h
@@ -0,0 +1,19 @@
+//
+//  OpenSSL-macOS.h
+//  OpenSSL-macOS
+//
+//  Created by Josip Cavar on 15/07/16.
+//  Copyright © 2016 krzyzanowskim. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OpenSSL-macOS.
+FOUNDATION_EXPORT double OpenSSL_macOSVersionNumber;
+
+//! Project version string for OpenSSL-macOS.
+FOUNDATION_EXPORT const unsigned char OpenSSL_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenSSL_macOS/PublicHeader.h>
+
+
