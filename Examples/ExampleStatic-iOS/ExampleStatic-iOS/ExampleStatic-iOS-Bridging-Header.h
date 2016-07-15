e8d6467d4c3009804623dea5484c203638793877
Josip Cavar <josipcavar04@gmail.com>
2016-07-15 23:29:58 +0200

    update build script

---
+++ Examples/ExampleStatic-iOS/ExampleStatic-iOS/ExampleStatic-iOS-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  ExampleStatic-iOS-Bridging-Header.h
+//  ExampleStatic-iOS
+//
+//  Created by Josip Cavar on 15/07/16.
+//  Copyright © 2016 krzyzanowskim. All rights reserved.
+//
+
+#ifndef ExampleStatic_iOS_Bridging_Header_h
+#define ExampleStatic_iOS_Bridging_Header_h
+
+
+#endif /* ExampleStatic_iOS_Bridging_Header_h */
