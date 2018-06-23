f5e7464e26fbf6528ff15a86e92990550c456a11
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2018-06-23 12:34:21 +0100

    Add modulemap

--- create-framework.sh
+++ create-framework.sh
@@ -2,7 +2,7 @@
 
 # Bitcode is not working
 
-FWNAME="openssl"
+FWNAME="OpenSSL"
 OSX_MIN="10.9"
 IOS_MIN="8.0"
 
@@ -23,5 +23,27 @@ xcrun --sdk iphoneos libtool -dynamic -no_warning_for_no_symbols -undefined dyna
 xcrun --sdk macosx   libtool -dynamic -no_warning_for_no_symbols -undefined dynamic_lookup -macosx_version_min $OSX_MIN -o Frameworks/macos/$FWNAME.framework/$FWNAME lib-macos/libcrypto.a lib-macos/libssl.a
 
 cp -r include-ios/$FWNAME/* Frameworks/ios/$FWNAME.framework/Headers/
+sed -i '' 's/openssl/OpenSSL/' Frameworks/ios/$FWNAME.framework/Headers/*.h
+
 cp -r include-macos/$FWNAME/* Frameworks/macos/$FWNAME.framework/Headers/
+sed -i '' 's/openssl/OpenSSL/' Frameworks/macos/$FWNAME.framework/Headers/*.h
+
+echo "Create module OpenSSL"
+mkdir -p Frameworks/ios/$FWNAME.framework/Modules
+mkdir -p Frameworks/macos/$FWNAME.framework/Modules
+
+echo "framework module OpenSSL {
+    umbrella header \"OpenSSL.h\"
+
+    export *
+    module * { export *}
+}" > Frameworks/ios/$FWNAME.framework/Modules/module.modulemap
+
+echo "framework module OpenSSL {
+    umbrella header \"OpenSSL.h\"
+
+    export *
+    module * { export *}
+}" > Frameworks/macos/$FWNAME.framework/Modules/module.modulemap
+
 echo "Created $FWNAME.framework"
