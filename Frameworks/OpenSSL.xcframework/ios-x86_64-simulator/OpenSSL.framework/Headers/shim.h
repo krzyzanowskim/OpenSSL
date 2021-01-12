ac058d82b282e1fd8a1b7c74a98422b27120f508
M Ivaniushchenko <666tos@gmail.com>
2021-01-12 16:30:21 +0100

    using old shim.h

--- Frameworks/OpenSSL.xcframework/ios-x86_64-simulator/OpenSSL.framework/Headers/shim.h
+++ Frameworks/OpenSSL.xcframework/ios-x86_64-simulator/OpenSSL.framework/Headers/shim.h
@@ -8,34 +8,66 @@
 #include <openssl/x509.h>
 #include <openssl/cms.h>
 
-#undef SSL_library_init
-static inline void SSL_library_init() {
-    OPENSSL_init_ssl(0, NULL);
-}
-
-#undef SSL_load_error_strings
-static inline void SSL_load_error_strings() {
-    OPENSSL_init_ssl(OPENSSL_INIT_LOAD_SSL_STRINGS \
-                     | OPENSSL_INIT_LOAD_CRYPTO_STRINGS, NULL);
-}
-
-#undef OpenSSL_add_all_ciphers
-static inline void OpenSSL_add_all_ciphers() {
-    OPENSSL_init_crypto(OPENSSL_INIT_ADD_ALL_CIPHERS, NULL);
-}
-
-#undef OpenSSL_add_all_digests
-static inline void OpenSSL_add_all_digests() {
-    OPENSSL_init_crypto(OPENSSL_INIT_ADD_ALL_DIGESTS, NULL);
-}
-
-#undef OpenSSL_add_all_algorithms
-static inline void OpenSSL_add_all_algorithms() {
-    #ifdef OPENSSL_LOAD_CONF
-    OPENSSL_add_all_algorithms_conf();
-    #else
+// Initialize OpenSSL
+static inline void OpenSSL_SSL_init(void) {
+    SSL_library_init();
+    SSL_load_error_strings();
+    OPENSSL_config(NULL);
+    OpenSSL_add_all_ciphers();
     OPENSSL_add_all_algorithms_noconf();
-    #endif
 }
+
+static inline void EVP_PKEY_assign_wrapper(EVP_PKEY *pkey, RSA *rsakey) {
+    EVP_PKEY_assign(pkey, EVP_PKEY_RSA, rsakey);
+}
+
+static inline STACK_OF(CMS_SignerInfo) *STACK_CMS_SignerInfo_new_null() {
+  return SKM_sk_new_null(CMS_SignerInfo);
+}
+
+static inline void STACK_CMS_SignerInfo_free(STACK_OF(CMS_SignerInfo) *stack) {
+  SKM_sk_free(CMS_SignerInfo, stack);
+}
+
+static inline int STACK_CMS_SignerInfo_push(STACK_OF(CMS_SignerInfo) *stack, void *data) {
+  return SKM_sk_push(CMS_SignerInfo, stack, data);
+}
+
+static inline void *STACK_CMS_SignerInfo_pop(STACK_OF(CMS_SignerInfo) *stack) {
+  return SKM_sk_pop(CMS_SignerInfo, stack);
+}
+
+static inline STACK_OF(X509) *STACK_X509_new_null() {
+    return SKM_sk_new_null(X509);
+}
+
+static inline void STACK_X509_free(STACK_OF(X509) *stack) {
+    SKM_sk_free(X509, stack);
+}
+
+static inline int STACK_X509_push(STACK_OF(X509) *stack, void *data) {
+    return SKM_sk_push(X509, stack, data);
+}
+
+static inline void *STACK_X509_pop(STACK_OF(X509) *stack) {
+    return SKM_sk_pop(X509, stack);
+}
+
+static inline STACK_OF(X509) *STACK_X509_dup(STACK_OF(X509) *stack) {
+    return SKM_sk_dup(X509, stack);
+}
+
+static inline int STACK_X509_num(STACK_OF(X509) *stack) {
+    return SKM_sk_num(X509, stack);
+}
+
+static inline int STACK_X509_find(STACK_OF(X509) *stack, void *data) {
+    return SKM_sk_find(X509, stack, data);
+}
+
+static inline void STACK_X509_zero(STACK_OF(X509) *stack) {
+    SKM_sk_zero(X509, stack);
+}
+
 
 #endif
