680bc5d4e3807294e49246a22971b1de52023568
Marcin Krzyzanowski <marcin.krzyzanowski@gmail.com>
2024-04-28 20:21:26 +0200

    Build version 3.1.5002 with visionOS support

--- Project.swift
+++ Project.swift
@@ -1,7 +1,7 @@
 import ProjectDescription
 
 private let developmentTeam: SettingValue = "67RAULRX93"
-private let marketingVersion: String = "3.1.5001"
+private let marketingVersion: String = "3.1.5002"
 
 let project = Project(
     name: "OpenSSL",
